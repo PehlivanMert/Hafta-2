bb8709c81b0e1e1f2dfee73dcec48dddeae88314
PehlivanMert <pehlivannmert@gmail.com>
2023-12-18 03:19:08 +0300

    Palindrom Sayılar

---
+++ DeseneGoreMethotOlusturma/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Deseni bulmak için bir sayı girin: ");
+        int num = input.nextInt();
+        PatternCreator(num, num, -1);
+    }
+
+    static void PatternCreator(int sabitSayi, int degiskenSayi, int isaret)
+    {
+        if(degiskenSayi > sabitSayi) return;
+        if(degiskenSayi < 1) isaret *= -1;
+        System.out.print(" " + degiskenSayi + " ");
+        PatternCreator(sabitSayi, degiskenSayi  + isaret * 5, isaret);
+    }
+}
--- PalindromSayilar/src/Main.java
+++ PalindromSayilar/src/Main.java
@@ -1,6 +1,36 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
 
+public class Main {
+
+    static boolean isPalindrom(int number) {
+
+        int temp = number, reverseNumber = 0, lastNumber;
+
+        while (temp != 0) {
+            //System.out.println("=================");
+
+            lastNumber = temp % 10;
+            //System.out.println("son sayi : " + lastNumber);
+
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            //System.out.println(" tersi ; " + reverseNumber);
+
+            temp /= 10;
+            //System.out.println("islemden sonra ki yeni sayi :" + temp);
+        }
+
+        if (number == reverseNumber)
+        {return true;
+        } else{
+            return false ;
+        }
     }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayıyı giriniz : ");
+        int number = input.nextInt();
+        System.out.println(isPalindrom(number));
+    }
+
 }
