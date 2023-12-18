51d5c86fb7a4a36457004885f0d00400bfdc1eab
PehlivanMert <pehlivannmert@gmail.com>
2023-12-18 03:45:24 +0300

    Açıklama eklendi

--- DeseneGoreMethotOlusturma/src/Main.java
+++ DeseneGoreMethotOlusturma/src/Main.java
@@ -8,11 +8,14 @@ public class Main {
         PatternCreator(num, num, -1);
     }
 
+    /* sabitSayi: Desenin kaç adet sayı içereceğini belirten sayı.
+    degiskenSayi: Desende kullanılan değişken sayıdır. Bu sayı, her çağrıda isaret değeri ile artırılır veya azaltılır.
+    isaret: Desenin nasıl arttırılacağını veya azaltılacağını belirten bir işaret değeridir. */
     static void PatternCreator(int sabitSayi, int degiskenSayi, int isaret)
     {
-        if(degiskenSayi > sabitSayi) return;
-        if(degiskenSayi < 1) isaret *= -1;
-        System.out.print(" " + degiskenSayi + " ");
-        PatternCreator(sabitSayi, degiskenSayi  + isaret * 5, isaret);
+        if(degiskenSayi > sabitSayi) return;           // Eğer degiskenSayi sabitSayi'den büyükse, fonksiyon sona erer.
+        if(degiskenSayi < 1) isaret *= -1;             // Eğer degiskenSayi 1'den küçükse, isaret'in tersini alır.
+        System.out.print(" " + degiskenSayi + " ");    // Deseni ekrana yazdırırız.
+        PatternCreator(sabitSayi, degiskenSayi  + isaret * 5, isaret); // // Recursive olarak fonksiyonu çağırırız.
     }
 }
