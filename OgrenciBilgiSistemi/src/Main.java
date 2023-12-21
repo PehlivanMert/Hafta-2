30fd3001802ee0af7dde88a9b63cfa30a9da1a05
PehlivanMert <pehlivannmert@gmail.com>
2023-12-21 20:31:37 +0300

    Öğrenci Bilgi Sistemi

--- OgrenciBilgiSistemi/src/Main.java
+++ OgrenciBilgiSistemi/src/Main.java
@@ -22,14 +22,14 @@ public class Main {
         s1.isPass();
 
         Student s2 = new Student("Rabia","757","4",tarih,fizik,cografya);
-        s1.addBulkExamNote(70,90,95);
-        s1.addBulkOpinionNote(80,100,100);
-        s1.isPass();
+        s2.addBulkExamNote(70,90,95);
+        s2.addBulkOpinionNote(80,100,100);
+        s2.isPass();
 
         Student s3 = new Student("Hayri","123","4",tarih,fizik,cografya);
-        s1.addBulkExamNote(25,45,65);
-        s1.addBulkOpinionNote(50,70,85);
-        s1.isPass();
+        s3.addBulkExamNote(25,45,65);
+        s3.addBulkOpinionNote(50,70,85);
+        s3.isPass();
 
 
     }
